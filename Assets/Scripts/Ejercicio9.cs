42ea03801d9a59074d26abaa372d870c6d3caa18
Facu-Vaz <facundo.vazber@gmail.com>
2022-03-31 10:42:03 -0300

    Exercise 10 Finished

--- Assets/Scripts/Ejercicio9.cs
+++ Assets/Scripts/Ejercicio9.cs
@@ -2,6 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Realizá un programa que resuelva el siguiente problema:  
+//Tres personas aportan diferente capital a una sociedad.Se desea saber qué 
+//porcentaje del total aportó cada una (indicando nombre y porcentaje) 
+//y cuál es el monto del total aportado  por las tres.
+
+//Ingresar por Inspector el ingreso del capital aportado por cada una de las personas y 
+//luego  mostrar lo pedido en el siguiente formato:  
+//Nombre: capital aportado: $ … , Porcentaje del capital: %.... , Monto total aportado: $ ….
+
+
 public class Ejercicio9 : MonoBehaviour
 {
     public string nombre1;
